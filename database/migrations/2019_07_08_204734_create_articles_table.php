457b7b0710e39d22e250fa12bc5b02fd1c095f12
“her-cat” <hxhsoft@foxmail.com>
2019-07-08 21:08:05 +0800

    生成文章模型

---
+++ database/migrations/2019_07_08_204734_create_articles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->index()->comment('用户 ID');
+            $table->bigInteger('category_id')->index()->comment('分类 ID');
+            $table->string('slug')->nullable()->comment('SEO 友好的 URI');
+            $table->string('title')->index()->comment('标题');
+            $table->string('excerpt')->nullable()->comment('摘要');
+            $table->text('content')->comment('内容');
+            $table->unsignedInteger('reply_count')->default(0)->comment('回复数量');
+            $table->unsignedInteger('view_count')->default(0)->comment('浏览量');
+            $table->timestamp('published_at')->nullable()->comment('发布时间');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
