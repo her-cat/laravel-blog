14487f9fc98078d0d8d73eb76a56a58dca7d05b5
“her-cat” <hxhsoft@foxmail.com>
2019-07-10 00:06:28 +0800

    修复 XSS 注入漏洞

--- app/Observers/ArticleObserver.php
+++ app/Observers/ArticleObserver.php
@@ -8,6 +8,8 @@ class ArticleObserver
 {
     public function saving(Article $article)
     {
+        $article->content = clean($article->content, 'user_article_body');
+
         $article->excerpt = make_excerpt($article->content);
     }
 }
