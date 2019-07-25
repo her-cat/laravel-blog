4e737c4afdc553011fa5fd14d851355367d9a90f
何湘辉 <hxhsoft@foxmail.com>
2019-07-25 16:37:33 +0800

    安装 baidu-translator

--- app/Handlers/SlugTranslateHandler.php
+++ app/Handlers/SlugTranslateHandler.php
@@ -3,6 +3,7 @@
 namespace App\Handlers;
 
 use GuzzleHttp\Client;
+use HerCat\BaiduTranslator\BaiduTranslator;
 use Illuminate\Support\Str;
 use Overtrue\Pinyin\Pinyin;
 
@@ -10,31 +11,7 @@ class SlugTranslateHandler
 {
     public function translate($text)
     {
-        $http = new Client();
-
-        $api = 'http://api.fanyi.baidu.com/api/trans/vip/translate?';
-        $appid = config('services.baidu_translate.appid');
-        $key = config('services.baidu_translate.key');
-        $salt = time();
-
-        if (empty($appid) || empty($key)) {
-            return $this->pinyin($text);
-        }
-
-        $sign = md5($appid.$text.$salt.$key);
-
-        $query = http_build_query([
-            'q' => $text,
-            'from' => 'zh',
-            'to' => 'en',
-            'appid' => $appid,
-            'salt' => $salt,
-            'sign' => $sign,
-        ]);
-
-        $response = $http->get($api.$query);
-
-        $result = json_decode($response->getBody(), true);
+        $result = app(BaiduTranslator::class)->translate($text, 'en');
 
         if (isset($result['trans_result'][0]['dst'])) {
             return Str::slug($result['trans_result'][0]['dst']);
---
+++ config/translator.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'app_id' => env('BAIDU_TRANSLATE_APPID'),
+    'key' => env('BAIDU_TRANSLATE_KEY'),
+];
